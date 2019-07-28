5074ac8d9efd97cfdf7c83213e2b91979a0483ff
Daniel Mecke <daniel.mecke@cunningsoft.de>
2019-07-28 21:21:54 +0200

    use vuetify / vue

---
+++ src/components/Project.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-hover #default="{ hover }">
+        <v-card
+            outlined
+            :href="url"
+        >
+            <v-img
+                :src="require('@/assets/projects/' + image)"
+                height="400px"
+                class="white--text"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            >
+                <v-card-title class="fill-height align-end">{{ name }}</v-card-title>
+            </v-img>
+            <v-card-actions>
+                <p-state :state="state" />
+            </v-card-actions>
+        </v-card>
+    </v-hover>
+</template>
+
+<script>
+    import State from "./State";
+
+    export default {
+        name: 'p-project',
+        components: {
+            'p-state': State,
+        },
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+            image: {
+                type: String,
+                required: true,
+            },
+            state: {
+                type: String,
+                required: true,
+            },
+            url: {
+                type: String,
+                required: true,
+            },
+        },
+    }
+</script>
---
+++ src/components/State.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-chip :color="color" :dark="dark" small>
+        <v-icon left small>{{ icon }}</v-icon>
+        {{ state }}
+    </v-chip>
+</template>
+
+<script>
+    export default {
+        name: 'p-state',
+        props: {
+            state: {
+                type: String,
+                required: true,
+            },
+        },
+        computed: {
+            color() {
+                switch (this.state) {
+                    case 'production':
+                        return 'green';
+
+                    case 'alpha':
+                        return 'blue';
+
+                    case 'prototype':
+                        return 'amber';
+                }
+
+                return '';
+            },
+            dark() {
+                switch (this.state) {
+                    case 'production':
+                    case 'alpha':
+                        return true;
+                }
+                return false;
+            },
+            icon() {
+                switch (this.state) {
+                    case 'production':
+                        return 'mdi-flag';
+
+                    case 'alpha':
+                        return 'mdi-content-cut';
+
+                    case 'prototype':
+                        return 'mdi-wrench';
+                }
+
+                return '';
+            },
+        }
+    }
+</script>
